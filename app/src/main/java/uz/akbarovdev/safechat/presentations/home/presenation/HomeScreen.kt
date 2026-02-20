e4108444d6d841f7fb3459634866e5d78f9f279e
Akhmadjon Akbarov <akhmadjonakbarov@gmail.com>
2026-02-21 00:02:55 +0500

    updated

--- app/src/main/java/uz/akbarovdev/safechat/presentations/home/presenation/HomeScreen.kt
+++ app/src/main/java/uz/akbarovdev/safechat/presentations/home/presenation/HomeScreen.kt
@@ -1,35 +1,22 @@
 package uz.akbarovdev.safechat.presentations.home.presenation
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.Notifications
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -37,31 +24,27 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.koin.compose.viewmodel.koinViewModel
+import uz.akbarovdev.safechat.core.design_system.ui.theme.SafeChatTheme
 import uz.akbarovdev.safechat.presentations.home.domain.models.ChatRoom
 import uz.akbarovdev.safechat.presentations.home.domain.models.ChatRoomDetails
 import uz.akbarovdev.safechat.presentations.home.domain.models.ChatRoomUser
+import uz.akbarovdev.safechat.presentations.home.presenation.components.ChatItem
+import uz.akbarovdev.safechat.presentations.home.presenation.components.UserItem
 import uz.akbarovdev.safechat.presentations.home.presenation.view_model.HomeAction
 import uz.akbarovdev.safechat.presentations.home.presenation.view_model.HomeState
 import uz.akbarovdev.safechat.presentations.home.presenation.view_model.HomeViewModel
-import uz.akbarovdev.safechat.core.design_system.ui.theme.SafeChatTheme
-import uz.akbarovdev.safechat.presentations.home.presenation.components.ChatItem
 
 @Composable
 fun HomeRoot(
@@ -166,23 +149,48 @@ fun HomeScreen(
                     )
                     Spacer(modifier = Modifier.height(20.dp))
 
+                    if (state.chatRooms.isNotEmpty() && state.users.isEmpty()) {
+                        LazyColumn {
+                            items(state.chatRooms) { chatRoom ->
+                                ChatItem(
+                                    Modifier.padding(vertical = 10.dp),
+                                    chatRoom
+                                ) {
+                                    onNavigateToChat(
+                                        ChatRoomDetails(
+                                            roomId = chatRoom.id,
+                                            receiverId = chatRoom.receiver.id,
+                                            receiverName = chatRoom.receiver.email,
 
-                    LazyColumn {
-                        items(state.chatRooms) { chatRoom ->
-                            ChatItem(
-                                Modifier.padding(vertical = 10.dp),
-                                chatRoom
-                            ) {
-                                onNavigateToChat(
-                                    ChatRoomDetails(
-                                        roomId = chatRoom.id,
-                                        receiverId = chatRoom.receiver.id,
-                                        receiverName = chatRoom.receiver.email,
+                                        )
                                     )
-                                )
+                                }
+                            }
+                        }
+                    } else {
+                        LazyColumn {
+                            items(state.users) { user ->
+                                UserItem(
+                                    Modifier.padding(vertical = 10.dp),
+                                    user
+                                ) {
+                                    onAction(HomeAction.OnCreateChatRoom(user.id))
+                                    if (state.createdChatRoomId != -1) {
+                                        onNavigateToChat(
+                                            ChatRoomDetails(
+                                                roomId = state.createdChatRoomId,
+                                                receiverId = user.id,
+                                                receiverName = user.email,
+
+                                            )
+                                        )
+                                    }
+                                }
                             }
                         }
                     }
+
+
                 }
             }
         }
@@ -200,7 +208,7 @@ private fun PreviewHome() {
                     ChatRoom(
                         1, "Salom qalesiz?", 0, ChatRoomUser(
                             1,
-                            "akhmad@gmail.com",
+                            "akhmad@gmail.com", "admin"
                         )
                     )
                 )
